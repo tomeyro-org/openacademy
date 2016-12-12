60c5c941d511524a39b4aa531a319545c25acc4d
Tomas Alvarez <tomas@vauxoo.com>
2016-12-12 15:53:30 +0000

    [ADD] added openacademy module definition

---
+++ openacademy/__openerp__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Open Academy",
+
+    'summary': """Manage trainings""",
+
+    'description': """
+	# Open Academy module for managing trainings:
+            - training courses
+            - training sessions
+            - a ttendees registration
+    """,
+
+    'author': "tomas@vauxoo.com",
+    'website': "http://www.vauxoo.com",
+
+    'category': "Test",
+    'version': "0.1",
+
+    'depends': ["base"],
+
+    'data': [
+        #
+    ],
+    'demo': [
+        #
+    ],
+
+    'installable': True,
+    'auto_install': False,
+}
