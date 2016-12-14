69f7cb59d8f23a8534a68f7e79e0c80583a6ddeb
Tomas Alvarez <tomas@vauxoo.com>
2016-12-14 16:12:04 +0000

    [REF] openacademy: adding domains to instructor field in sessions

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -11,7 +11,15 @@ class Session(models.Model):
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
 
-    instructor_id = fields.Many2one('res.partner', string='Instructor')
+    instructor_id = fields.Many2one(
+        'res.partner',
+        string='Instructor',
+        domain=[
+            '|',
+            ('instructor', '=', True),
+            ('category_id', 'ilike', 'Teacher'),
+        ]
+    )
     course_id = fields.Many2one(
         'openacademy.course',
         ondelete='cascade',
