5cbd64e3d57fcef66bf01846e8678a97a7941548
Tomas Alvarez <tomas@vauxoo.com>
2016-12-14 16:52:53 +0000

    [REF] openacademy: Adding computed field 'taken seats' to session model and view

--- openacademy/model/openacademy_session.py
+++ openacademy/model/openacademy_session.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from openerp import models, fields
+from openerp import models, fields, api
 
 
 class Session(models.Model):
@@ -10,6 +10,8 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+
+    taken_seats = fields.Float(compute="_taken_seats", default=0.0)
 
     instructor_id = fields.Many2one(
         'res.partner',
@@ -27,3 +29,11 @@ class Session(models.Model):
         required=True
     )
     attendee_ids = fields.Many2many('res.partner', string='Attendees')
+
+    @api.one
+    @api.depends('seats', 'attendee_ids')
+    def _taken_seats(self):
+        if self.seats:
+            self.taken_seats = 100 * len(self.attendee_ids) / self.seats
+        else:
+            self.taken_seats = 0
