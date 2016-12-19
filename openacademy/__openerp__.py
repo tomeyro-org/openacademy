b43ea31c64c89ce4be38533cafb8ced0bb04119e
Tomas Alvarez <tomas@vauxoo.com>
2016-12-19 18:03:40 +0000

    [ADD] openerp: create session report

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -21,6 +21,7 @@
         'security/security.xml',
         'security/ir.model.access.csv',
         'view/openacademy_wizard_view.xml',
+        'report/openacademy_session_report.xml',
     ],
     'demo': [
         'demo/openacademy_course_demo.xml',
